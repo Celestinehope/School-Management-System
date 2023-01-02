da386992d285a711cbcf7f62551772df98df035d
Celestinehope <98664134+Celestinehope@users.noreply.github.com>
2023-01-02 23:27:43 +0300

    sms

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'user_type',
+        'course',
+        'religion',
+        'high_school',
+        'time',
+        'DOB',
+        'phone_number',
+        'country',
+        'status',
+        'semester',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function classes() {
+        return $this->hasMany(Classes::class, 'user_id');
+    }
+    public function studentClasses() {
+        return $this->hasMany(studentClasses::class, 'user_id');
+    }
+}
